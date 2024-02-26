8a96b82faf7e14a4b11a916a55b43a3b308d4a46
KaylaOates <oatesk@ufl.edu>
2024-02-26 13:47:37 -0500

    frontend and backend are connected

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+from reverseProxy import proxyRequest
+
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = 'http://localhost:3000/'
+
+app = Flask(__name__)
+
+# Ignore static folder in development mode.
+if MODE == "development":
+    app = Flask(__name__, static_folder=None)
+
+@app.route('/')
+@app.route('/<path:path>')
+def index(path=''):
+    if MODE == 'development':
+        return proxyRequest(DEV_SERVER_URL, path)
+    else:
+        return render_template("index.html")    
